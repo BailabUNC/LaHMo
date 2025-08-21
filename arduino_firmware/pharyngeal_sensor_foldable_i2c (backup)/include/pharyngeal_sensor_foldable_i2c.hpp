b1adf44d633bc5ea2afc156ffb73d6a2a4e1b974
yihan-liu <y1hliu@unc.edu>
2025-08-21 17:47:07 -0400

    delete old arduino codes

--- arduino_firmware/pharyngeal_sensor_foldable_i2c (backup)/include/pharyngeal_sensor_foldable_i2c.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef PHARYNGEAL_SENSOR_FOLDABLE_HPP
-#define PHARYNGEAL_SENSOR_FOLDABLE_HPP
-
-extern "C"
-{
-    #include "pharyngeal_sensor_foldable.h"
-}
-
-#endif // PHARYNGEAL_SENSOR_FOLDABLE_HPP
